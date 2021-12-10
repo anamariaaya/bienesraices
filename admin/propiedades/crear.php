7d108a1dd35b156f7e43ac639904cda6c77e54f0
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2021-12-10 20:57:54 +0100

    Conexión a Base de datos
    Se genera la conexión a la base de datos local

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,9 @@
 <?php
+    // Base de datos
+    require '../../includes/config/database.php';
+    
+    $db = conectarDB();
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -13,17 +18,43 @@
                 <legend>Información General</legend>
 
                 <label for="titulo">Título:</label>
-                <input type="text" id="titulo" placeholder="Título Propiedad">
+                <input type="text" id="titulo" placeholder="Título Propiedad" required>
 
                 <label for="precio">Precio:</label>
-                <input type="number" id="precio" placeholder="Precio Propiedad">
+                <input type="number" id="precio" placeholder="Precio Propiedad" required>
 
                 <label for="imagen">Imagen:</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripción:</label>
-                <textarea id="descripcion"></textarea>
+                <textarea id="descripcion" required></textarea>
             </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej:3" min="1" max="9" required>
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej:3" min="1" max="9" required>
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej:3" min="0" max="9" required>
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option selected disabled>-- Elegir vendedor</option>
+                    <option value="1">Ana</option>
+                    <option value="2">Andreas</option>
+                    <option value="3">Juanjo</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton-amarillo">
         </form>
     </main>
 
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -26,10 +26,10 @@
                 <div class="derecha">
                     <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="ícono dark mode">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros.php">Nosotros</a>
+                        <a href="/anuncios.php">Anuncios</a>
+                        <a href="/blog.php">Blog</a>
+                        <a href="/contacto.php">Contacto</a>
                     </nav>
                 </div>
             </div>
